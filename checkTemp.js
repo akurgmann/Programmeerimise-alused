5952a9183db1e047133da63c0e3d4495ea4b3c1e
Anželika Kurgmann <115346227+akurgmann@users.noreply.github.com>
2022-10-18 15:12:42 +0300

    Add files via upload

---
+++ checkTemp.js
@@ -0,0 +1,10 @@
+function checkTemp (HetkeTemp) {
+  if (HetkeTemp >= 0 && HetkeTemp <= 20) {
+    return -1 }
+  if (HetkeTemp >= 21 && HetkeTemp <= 40) {
+  return 0 }
+  if (HetkeTemp >= 41 && HetkeTemp <= 60) {
+  return 1 }
+}
+
+
